cc99513181c51998755084e7bc35ce518c2e703a
Tyuiu.DolganovAA@outlook.com <Tyuiu.DolganovAA@outlook.com>
2024-10-04 18:03:43 +0300

    Task2

---
+++ Tyuiu.DolganovAA.Sprint1.Task2.V10.Lib/DataService.cs
@@ -0,0 +1,11 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint1;
+namespace Tyuiu.DolganovAA.Sprint1.Task2.V10.Lib
+{
+    public class DataService : ISprint1Task2V10
+    {
+        public double ConvertMetreToInchs(int value)
+        {
+            return value * 3.28;
+        }
+    }
+}
---
+++ Tyuiu.DolganovAA.Sprint1.Task2.V10.Test/DataServiceTest.cs
@@ -0,0 +1,16 @@
+using Tyuiu.DolganovAA.Sprint1.Task2.V10.Lib;
+namespace Tyuiu.DolganovAA.Sprint1.Task2.V10.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ConvertMetreToInchs()
+        {
+            DataService ds = new DataService();
+            int x = 5;
+            var res = ds.ConvertMetreToInchs(x);
+            Assert.AreEqual(16.40, res);
+        }
+    }
+}
